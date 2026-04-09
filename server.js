a88a1eab09e06620fc126e2a5739a171a03c42c5
btxtd212004-lgtm <btxtd212004@gmail.com>
2026-04-09 22:29:37 +0700

    increase body size limit for image upload

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 app.use('/api/auth',         require('./routes/auth'));
 app.use('/api/admin',        require('./routes/admin'));
